4f4d3d74acc53300fc726a107a0c61c7af2cd9b8
Raas-Lobby <robinsdev2@gmail.com>
2025-08-09 14:38:59 +0200

    added the login and register form

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,4 +1,32 @@
 <?php include '../../includes/header.php' ; ?>
-    <h1 class = "bold-title " >Login below</h1>
+    <h1 class = "mt" >Login below</h1>
+        <div class = "form-group ">
+     <form action = "register.php" method = "POST" class = "form mt-r">
+           
+             
+                <div class = "input-group">
+                <label for = "email">Email:</label>
+                <input type = "text" name = "email" class = "input-bar" placeholder = "Enter your email" required>
+                </div>
+
+                <div class = "input-group">
+            <label for = "password">Password:</label>
+                <input type = "text" name = "password" class = "input-bar" placeholder = "Enter your password" required>
+                </div>
+              
+</form>
+
+    <div class = "below-btm mt-r">
+        <div class = "button-container">
+            <button type = "submit" class = "button">Login</button>
+           
+        </div>
+        <div class = "button-container">
+    <a href = "register.php" class = "button">Register</a>
+        </div>
+    </div>
+   
+        
+</div>
 
 <?php include '../../includes/footer.php' ; ?>
--- views/auth/register.php
+++ views/auth/register.php
@@ -1,4 +1,50 @@
 <?php include '../../includes/header.php' ; ?>
-    <h1 class = "bold-title " >Register below</h1>
+    <h1 class = " mt " >Register below</h1>
+    <div class = "form-group">
+     <form action = "register.php" method = "POST" class = "form">
+           <div class = "input-group">
+              <label for = "firstName">First Name:</label>
+            <input type = "text" name = "firstName" class = "input-bar" placeholder = "Enter your first name" required>
+           </div>
+           
+              <div class = "input-group">
+               <label for = "lastName">Last Name:</label>
+            <input type = "text" name = "lastName" class = "input-bar" placeholder = "Enter your last name" required>
+              </div>
+            
+                <div class = "input-group">
+                <label for = "institute">Institute:</label>
+                <input type = "text" name = "Institute" class = "input-bar" placeholder = "Enter your Institute" required>
+                </div>
+                            
+                <div class = "input-group">
+                <label for = "studentID">Student ID:</label>
+                <input type = "text" name = "studentID" class = "input-bar" placeholder = "Enter your student ID" required>
+                </div>
+             
+                <div class = "input-group">
+                <label for = "email">Email:</label>
+                <input type = "text" name = "email" class = "input-bar" placeholder = "Enter your email" required>
+                </div>
 
+                <div class = "input-group">
+            <label for = "password">Password:</label>
+                <input type = "text" name = "password" class = "input-bar" placeholder = "Enter your password" required>
+                </div>
+              
+</form>
+
+    <div class = "below-btm">
+        <div class = "button-container">
+            <button type = "submit" class = "button">Register</button>
+           
+        </div>
+        <div class = "button-container">
+    <a href = "login.php" class = "button">Login</a>
+        </div>
+    </div>
+   
+        
+</div>
+        
 <?php include '../../includes/footer.php' ; ?>
