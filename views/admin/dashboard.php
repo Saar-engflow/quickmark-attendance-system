64350bf3c1781d4a887b4a7620c1e6a69363c674
Raas-Lobby <robinsdev2@gmail.com>
2025-08-14 22:20:02 +0200

    admin dashboard ui done , the whole QuickMark web ui done

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -31,6 +31,77 @@
   </nav>
 </header>
 <section>
-  <h1>Admin dashboard</h1>
+  <div  class = admin-container>
+    <div class = "header-sec flex-c">
+    <h1>hey Admin ;]</h1>
+  <p>there are currently x number of QuickMark users</p>
+    </div>
+
+    <div class = "counts-container flex-c">
+      
+         <div class = "count flex-c">
+          <div class ="count-title">
+ <h4 class = "">Students</h4>
+          </div>
+           <p class="count-num">0</p>
+         </div>
+
+                  <div class = "count flex-c">
+          <div class ="count-title">
+ <h4 class = "">Lecturers</h4>
+          </div>
+           <p class="count-num">0</p>
+         </div>
+
+                  <div class = "count flex-c">
+          <div class ="count-title">
+ <h4 class = "">Admins</h4>
+          </div>
+           <p class="count-num">1</p>
+         </div>
+    </div>
+
+     <div class = "counts-container flex-c">
+
+      <div class = "user-m flex-c">
+        <div class= "user-m-header flex-c">
+         <h4>User Requests</h4>
+        </div>
+        <div class ="user-m-cont flex-c">
+         <p>nothing yet</p>
+        </div>
+        
+        <div class = "user-m-footer flex-c">
+          <button>open</button>
+        </div>
+        </div>
+
+      <div class = "user-m flex-c">
+        <div class= "user-m-header flex-c">
+     <h4>User Management</h4>
+        </div>
+        <div class ="user-m-cont flex-c">
+         <p>nothing yet</p>
+        </div>
+        <div class = "user-m-footer flex-c">
+          <button>open</button>
+        </div>
+        </div>
+
+      <div class = "user-m flex-c">
+        <div class= "user-m-header flex-c">
+      <h4>System logs</h4>
+        </div>
+        <div class ="user-m-cont flex-c">
+         <p>nothing yet</p>
+        </div>
+     <div class = "user-m-footer flex-c">
+          <button>open</button>
+        </div>
+    </div>
+     
+</div>
+  </div>
+  
 </section>
 <?php include '../../includes/footer.php' ; ?>
