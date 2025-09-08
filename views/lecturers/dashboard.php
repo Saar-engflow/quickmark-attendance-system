9681f5f99283ca8830438b58287e1fd68c5cfd03
Raas-Lobby <robinsdev2@gmail.com>
2025-09-08 15:23:41 +0200

    lecturer dashboard ui

--- views/lecturers/dashboard.php
+++ views/lecturers/dashboard.php
@@ -44,7 +44,67 @@ $lecturer_id = $_SESSION['user_id'] ?? 'N/A';
 </header>
 
 <section>
- <h1>Dashboard</h1>
+  <div class = "flex-c move m-top2 ">
+ 
+<h1>hey  <?php echo htmlspecialchars($lecturer_firstName) ?> ;]</h1>
+
+<!-- top-deck cards -->
+<div class = "dashboard-card">
+ <!--Courses conducted  -->
+ <div class = " flex-c l-card-container">
+    <h4>Courses Conducted:</h4>
+    <!-- coURSES num -->
+    <p class = "count-num mt">0</p>
+    <div class = " flex-c add-course"><p>open</p></div>
+ </div>
+
+  <!-- attendance percentage -->
+  <div class = " flex-c l-card-container">
+    <h4>Attendance Percentage</h4>
+    <p class = "count-num mt">0%</p>
+    <div class = " flex-c add-course"><p>open</p></div>
+  </div>
+    
+  <!-- Recent Activity -->
+   <div class = "flex-c l-card-container">
+     <h4>Attendance Percentage</h4>
+     <div class = "flex-c">
+      <p class = " m-top2 ">last attendance marked at </p>
+      <p>00/00/00 -00:00hrs</p>
+    </div>
+     <div 
+     onclick = "window.location.href = 'mark_attendance.php'"
+     class = " flex-c add-course mt mt"><p>Mark Attendance</p></div>
+   </div>
+ 
+</div>
+<!-- bottom-deck cards -->
+<div class = "dashboard-card">
+ <!-- Least attending students -->
+ <div class = " flex-c l-card-container">
+   <h4>least attending</h4>
+   <p>Students:</p>
+      <p class = "count-num mt">0</p>
+    <div class = " flex-c add-course"><p>open</p></div>
+ </div>
+ <!-- total number of students -->
+  <div class = " flex-c l-card-container">
+    <h4>Total </h4>
+   <p>Students:</p>
+      <p class = "count-num mt">0</p>
+    <div class = " flex-c add-course"><p>open</p></div>
+  </div>
+
+   <div class = " flex-c l-card-container">
+    <h4>Admin</h4>
+   <p>Notifications:</p>
+      <p class = "count-num mt">0</p>
+    <div class = " flex-c add-course"><p>open</p></div>
+   </div>
+ 
+</div>
+
+
 
  <!-- sidebar -->
   
@@ -77,7 +137,7 @@ $lecturer_id = $_SESSION['user_id'] ?? 'N/A';
    onclick = "window.location.href = 'reports.php'"
    class = "optn flex-c">Reports</div> 
 </div> 
-
+</div>
 </section>
 
 <?php include '../../includes/footer.php' ; ?>
